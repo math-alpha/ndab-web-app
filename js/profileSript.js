de4d802776e697fefa33f02814682a850a41e4e4
Ngadou Sylvestre <mathalpha26@gmail.com>
2019-03-16 11:08:20 +0100

    initial setup

---
+++ js/profileSript.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+function nextLesson() {
+
+    var image = document.getElementById('videoTag');
+    
+    if (image.src.match("vid/01")) {
+        image.src = "vid/2.mp4";
+    } else {
+        image.src = "vid/2.mp4";
+    }
+}
+
+
+
+
+
+
+
+
+
+
